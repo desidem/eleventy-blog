61e449cc54556c2f408b2a4c02d83f64c681b517
desidem <desdemyer@gmail.com>
2021-11-21 10:17:43 -0500

    price

--- functions/checkout-session.js
+++ functions/checkout-session.js
@@ -25,10 +25,22 @@ exports.handler = async (_event, context) => {
   const priceId = '{ price_1JtNOFACG3Zsu2XFU4il0Ki3 }'; 
 
   const link = await stripe.checkout.sessions.create({
-    price: priceId,
-    return_url: process.env.URL,
-  });
-
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'T-shirt',
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: 'https://yeuxandi.com/success',
+    cancel_url: 'https://yeuxandi.com/cancel',
+    });
   return {
     statusCode: 200,
     body: JSON.stringify(link.url),
@@ -36,3 +48,4 @@ exports.handler = async (_event, context) => {
 };
 
 console.log("well?");
+
