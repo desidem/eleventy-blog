19dfeb679d24ea8173d43707ca032cd037b47497
desidem <desdemyer@gmail.com>
2021-11-20 22:49:39 -0500

    stripe test

--- functions/checkout-session.js
+++ functions/checkout-session.js
@@ -1,17 +1,22 @@
-const stripe = require('stripe')('STRIPE_SECRET_KEY');
+const stripe = require('stripe')(process.env.'STRIPE_SECRET_KEY');
 /**const express = require('express');
 const app = express();
 app.use(express.static('public')); **/
 
-const YOUR_DOMAIN = 'http://yeuxandi.com';
+const priceId = '{{PRICE_ID}}'
+//const YOUR_DOMAIN = 'http://yeuxandi.com';
 
-app.post('/create-checkout-session', async (req, res) => {
+/** app.post('/create-checkout-session', async (req, res) => { **/
+  /** exports.handler = async (event) => { **/
   const session = await stripe.checkout.sessions.create({
-    customer_email: 'customer@example.com',
-    billing_address_collection: 'auto',
+    mode: 'subscription',
+   /** customer_email: 'customer@example.com',
+    billing_address_collection: 'auto', **/
+
    /**  shipping_address_collection: {
       allowed_countries: ['US', 'CA'],
     }, **/
+
     line_items: [
       {
         // Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
@@ -25,6 +30,6 @@ app.post('/create-checkout-session', async (req, res) => {
   });
 
   res.redirect(303, session.url);
-});
 
-app.listen(4242, () => console.log('Running on port 4242'));
+/** 
+app.listen(4242, () => console.log('Running on port 4242')); **/
