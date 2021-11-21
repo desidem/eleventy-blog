d7ff150924c1d6c8e94a20167e3c17b5094987a5
desidem <desdemyer@gmail.com>
2021-11-21 10:11:24 -0500

    renamed links

--- functions/checkout-session.js
+++ functions/checkout-session.js
@@ -1,44 +1,36 @@
+/** was named create-manage-link **/
+
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const fetch = require('isomorphic-fetch');
-/**const express = require('express');
-const app = express();
-app.use(express.static('public')); **/
+const { faunaFetch } = require('./utils/fauna');
 
+exports.handler = async (_event, context) => {
+  const { user } = context.clientContext;
 
-
-exports.handler = async (event) => {
-const priceId = '{ price_1JtNOFACG3Zsu2XFU4il0Ki3 }'; 
-//const YOUR_DOMAIN = 'http://yeuxandi.com';
-
-/** app.post('/create-checkout-session', async (req, res) => { **/
-  /** exports.handler = async (event) => { **/
-const session = await stripe.checkout.sessions.create({
-    mode: 'subscription',
-   /** customer_email: 'customer@example.com',
-    billing_address_collection: 'auto', **/
-
-   /**  shipping_address_collection: {
-      allowed_countries: ['US', 'CA'],
-    }, **/
-
-    line_items: [
-      {
-        // Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
-        price: price_1JtNOFACG3Zsu2XFU4il0Ki3,
-        quantity: 1,
-      },
-    ],
-  /**   mode: 'payment', **/
-    success_url: `{process.env.URL}/success.html`,
-    cancel_url: process.env.URL, 
+  const result = await faunaFetch({
+    query: `
+      query ($netlifyID: ID!) {
+        getUserByNetlifyID(netlifyID: $netlifyID) {
+          stripeID
+        }
+      }
+    `,
+    variables: {
+      netlifyID: user.sub,
+    },
   });
+
+  const { stripeID } = result.data.getUserByNetlifyID;
+  const priceId = '{ price_1JtNOFACG3Zsu2XFU4il0Ki3 }'; 
+
+  const link = await stripe.checkout.sessions.create({
+    price: priceId,
+    return_url: process.env.URL,
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(link.url),
+  };
 };
-  res.redirect(303, session.url);
 
-/** 
-app.listen(4242, () => console.log('Running on port 4242')); **/
-
-console.log("hiya"); 
-
-
-
+console.log("well?");
---
+++ functions/checkout-sessionX.js
@@ -0,0 +1,44 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const fetch = require('isomorphic-fetch');
+/**const express = require('express');
+const app = express();
+app.use(express.static('public')); **/
+
+
+
+exports.handler = async (event) => {
+const priceId = '{ price_1JtNOFACG3Zsu2XFU4il0Ki3 }'; 
+//const YOUR_DOMAIN = 'http://yeuxandi.com';
+
+/** app.post('/create-checkout-session', async (req, res) => { **/
+  /** exports.handler = async (event) => { **/
+const session = await stripe.checkout.sessions.create({
+    mode: 'subscription',
+   /** customer_email: 'customer@example.com',
+    billing_address_collection: 'auto', **/
+
+   /**  shipping_address_collection: {
+      allowed_countries: ['US', 'CA'],
+    }, **/
+
+    line_items: [
+      {
+        // Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+        price: price_1JtNOFACG3Zsu2XFU4il0Ki3,
+        quantity: 1,
+      },
+    ],
+  /**   mode: 'payment', **/
+    success_url: `{process.env.URL}/success.html`,
+    cancel_url: process.env.URL, 
+  });
+};
+  res.redirect(303, session.url);
+
+/** 
+app.listen(4242, () => console.log('Running on port 4242')); **/
+
+console.log("hiya"); 
+
+
+
