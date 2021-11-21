8909e14ee187b736c0078c640f9b321e836064eb
desidem <desdemyer@gmail.com>
2021-11-20 22:53:31 -0500

    key

--- functions/checkout-session.js
+++ functions/checkout-session.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.'STRIPE_SECRET_KEY');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 /**const express = require('express');
 const app = express();
 app.use(express.static('public')); **/
