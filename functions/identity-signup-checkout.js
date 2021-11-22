0c2db3daba5498981efc6d2549edebdc45ea926c
desidem <desdemyer@gmail.com>
2021-11-22 05:22:23 -0500

    added id back in

--- functions/identity-signup-checkout.js
+++ functions/identity-signup-checkout.js
@@ -17,7 +17,7 @@ exports.handler = async (event) => {
   // create a new customer in Stripe
 //  const customer = await stripe.customers.create({ email: user.email });
 //const customer = await stripe.customers.create({ email: user.email });
-//const customer = await stripe.customers.create({ id: user.id });
+const customer = await stripe.customers.create({ id: user.id });
   // subscribe the new customer to the free plan
 
   // store the Netlify and Stripe IDs in Fauna
@@ -32,6 +32,7 @@ exports.handler = async (event) => {
     `,
      variables: {
         netlifyID: user.id,
+        stripeID: customer.id,
       /** 
       netlifyID: user.id,
   /**    stripeID: subscription.customer, **/
