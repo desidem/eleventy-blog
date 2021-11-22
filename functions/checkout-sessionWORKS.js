e56c17316884502764d7eef057156ea2d14654ee
desidem <desdemyer@gmail.com>
2021-11-22 02:38:52 -0500

    changed checkout js

--- functions/checkout-sessionWORKS.js
+++ functions/checkout-sessionWORKS.js
@@ -1,29 +1,9 @@
 /** was named create-manage-link **/
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const { user } = require('./utils/fauna');
+const { faunaFetch } = require('./utils/fauna');
 
 exports.handler = async (_event, context) => {
   const { user } = context.clientContext;
-
-  const result = await faunaFetch({
-    query: `
-      query ($netlifyID: ID!) {
-        getUserByNetlifyID(netlifyID: $netlifyID) {
-          stripeID
-        }
-      }
-    `,
-    variables: {
-      netlifyID: user.sub,
-    },
-  });
-/** 
-  const { stripeID } = result.data.getUserByNetlifyID;
-**/
-
-
-
-
 
   const priceId = 'price_1JtNOFACG3Zsu2XFU4il0Ki3'; 
   
