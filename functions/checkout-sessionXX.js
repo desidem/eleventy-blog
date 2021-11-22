4c99ff1e03da15ca5c024998d79fdb82dde86d9f
desidem <desdemyer@gmail.com>
2021-11-21 19:33:43 -0500

    changes

---
+++ functions/checkout-sessionXX.js
@@ -0,0 +1,104 @@
+/** was named create-manage-link **/
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const { faunaFetch } = require('./utils/fauna');
+
+exports.handler = async (_event, context) => {
+  const { user } = context.clientContext;
+
+  await faunaFetch({
+    query: `
+      mutation ($netlifyID: ID!, $stripeID: ID!) {
+        createUser(data: { netlifyID: $netlifyID, stripeID: $stripeID }) {
+          netlifyID
+          stripeID
+        }
+      }
+    `,
+    variables: {
+      netlifyID: user.id,
+      stripeID: customer.id,
+    },
+  });
+/** 
+  const result = await faunaFetch({
+    query: `
+      query ($netlifyID: ID!) {
+        getUserByNetlifyID(netlifyID: $netlifyID) {
+          stripeID
+        }
+      }
+    `,
+    variables: {
+      netlifyID: user.sub,
+    },
+  });
+
+  const { stripeID } = result.data.getUserByNetlifyID;
+
+  **/
+  const priceId = 'price_1JtNOFACG3Zsu2XFU4il0Ki3'; 
+  
+  const link = await stripe.checkout.sessions.create({
+    
+    mode: 'subscription',
+    line_items: [
+      {
+        price: priceId,
+        // For metered billing, do not pass quantity
+        quantity: 1,
+      },
+    ],  
+    /** 
+    tax_rates: [
+      '{{TAX_RATE_ID}}',
+    ],**/
+    
+    automatic_tax: {
+      enabled: true,
+    }, 
+  /**   mode: 'payment', **/
+
+    success_url: 'https://yeuxandi.com/success-manage/index',
+    cancel_url: 'https://yeuxandi.com/index',
+
+
+    /** 
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'T-shirt',
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: 'https://yeuxandi.com/success',
+    cancel_url: 'https://yeuxandi.com/cancel',
+  **/
+
+    });
+/**  return {
+    statusCode: 200,
+    body: JSON.stringify(link.url),
+  }; **/
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      app_metadata: {
+        roles: ['pro'],
+      },
+    }),
+  };
+
+
+
+
+};
+
+console.log("well?");
+
