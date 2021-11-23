8b484bc8dbd5f08ee23cb56f97b855cb08feb3bb
desidem <desdemyer@gmail.com>
2021-11-22 23:40:08 -0500

    removed IDs

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -6,14 +6,15 @@ exports.handler = async (event) => {
   const { user } = JSON.parse(event.body);
 
 
- /**    const netlifyID = user.id; **/
+ /** const netlifyID = user.id; **/
   // create a new customer in Stripe
 //  const customer = await stripe.customers.create({ id: user.id }); //
 //const customer = await stripe.customers.create({ email: user.email });
   // subscribe the new customer to the free plan
 
   // store the Netlify and Stripe IDs in Fauna
-  const netlifyID = await faunaFetch({
+ /**  const netlifyID = **/
+ await faunaFetch({
     query: `
       mutation ($netlifyID: ID! ) {
         createUser(data: { netlifyID: $netlifyID }) {
