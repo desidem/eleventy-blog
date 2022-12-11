ac319193bc2c450e040bc6a437df4e4a2c9c8b69
desidem <desdemyer@gmail.com>
2022-12-11 02:02:13 -0500

    test batwing

---
+++ src/js/fable_test.js
@@ -0,0 +1,32 @@
+
+var animcontainer = document.getElementById('anim_container');
+
+var animData = {
+    container: animcontainer,
+	renderer: 'svg',
+	loop: false,
+	autoplay: true,
+	path: '/js/data batwing.json' 
+    /** 
+	
+	path: 'data211.json' 
+    path: 'data200.json' 
+    path: 'data21b.json' 
+    path: 'data21.json' **/
+};
+
+var anim = lottie.loadAnimation(animData);
+
+
+anim.addEventListener('DOMLoaded', firstLoop);
+
+
+/**THIS WORKS BUT LOOPS ON CLICK AT END--REMOVE FOR TEST ONLY **/
+
+function firstLoop() {
+    anim.playSegments([0,90], true);
+    animcontainer.addEventListener('click', secondLoop);
+                         
+};
+
+
