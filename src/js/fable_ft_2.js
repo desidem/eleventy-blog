f874cf54509d6709770fcb25251d4225de1ed2e6
desidem <desdemyer@gmail.com>
2023-06-12 19:40:27 -0400

    new honey 101 json file

--- src/js/fable_ft_2.js
+++ src/js/fable_ft_2.js
@@ -6,7 +6,7 @@ var animData = {
 	renderer: 'svg',
 	loop: false,
 	autoplay: true,
-	path: '/js/honey1 test2 data.json' 
+	path: '/js/honey_data_101.json' 
     /** 
 	path: '/js/data1000.json' 
 	path: 'data211.json' 
